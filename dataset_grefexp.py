3f945456879b39a8e3b84fcb0de3cbc4e5245744
Larry Neal <nealla@lwneal.com>
2017-05-05 12:30:34 -0700

    Add dataset_grefexp utility for data parsing

---
+++ dataset_grefexp.py
@@ -0,0 +1,65 @@
+import os
+import sys
+import json
+import random
+
+import redis
+import numpy as np
+import enchant
+
+DATA_DIR = '/home/nealla/data'
+
+KEY_GREFEXP_TRAIN = 'dataset_grefexp_train'
+KEY_GREFEXP_VAL = 'dataset_grefexp_val'
+
+conn = redis.Redis()
+categories = {v['id']: v['name'] for v in json.load(open('coco_categories.json'))}
+
+
+def example(reference_key=KEY_GREFEXP_TRAIN):
+    key = conn.srandmember(reference_key)
+    return get_annotation_for_key(key)
+
+
+def get_all_keys(reference_key=KEY_GREFEXP_VAL, shuffle=True):
+    keys = list(conn.smembers(reference_key))
+    if shuffle:
+        random.shuffle(keys)
+    return keys
+
+
+def get_annotation_for_key(key):
+    grefexp = json.loads(conn.get(key))
+    anno_key = 'coco2014_anno_{}'.format(grefexp['annotation_id'])
+    anno = json.loads(conn.get(anno_key))
+    img_key = 'coco2014_img_{}'.format(anno['image_id'])
+    img_meta = json.loads(conn.get(img_key))
+
+    img_width = float(img_meta['width'])
+    img_height = float(img_meta['height'])
+    x0, y0, width, height = anno['bbox']
+
+    box = (x0 / img_width, (x0 + width) / img_width, y0 / img_height, (y0 + height) / img_height)
+
+    texts = [g['raw'] for g in grefexp['refexps']]
+
+    #texts = [spell(strip(t, strip_end=False)) for t in texts]
+
+    category = categories[anno['category_id']]
+    return img_meta['filename'], box, category, texts
+
+
+if __name__ == '__main__':
+    img_filename = sys.argv[1]
+    sentence_filename = sys.argv[2]
+    fp_img = open(img_filename, 'w')
+    fp_sentence = open(sentence_filename, 'w')
+
+    for k in get_all_keys(KEY_GREFEXP_VAL):
+        filename, box, category, texts = get_annotation_for_key(k)
+        for sentence in texts:
+            img_info = {'filename': filename, 'bbox': box}
+            fp_img.write(json.dumps(img_info) + '\n')
+            fp_sentence.write(sentence.encode('utf-8') + '\n')
+    fp_img.close()
+    fp_sentence.close()

880ecc58ab9db9c33c7086d2f3381a2b54781215
Larry Neal <nealla@lwneal.com>
2017-05-05 11:10:26 -0700

    Move sampling functions to sampler.py

--- transcoder.py
+++ transcoder.py
@@ -46,24 +46,6 @@ def demonstrate(model, encoder_dataset, decoder_dataset, input_text=None, **para
         print('{} --> {}'.format(left, right))
 
 
-# Actually boltzmann(log(x)) for stability
-def boltzmann(pdf, temperature=1.0, epsilon=1e-5):
-    if temperature < epsilon:
-        return pdf / (pdf.sum() + epsilon)
-    pdf = np.log(pdf) / temperature
-    x = np.exp(pdf)
-    sums = np.sum(x, axis=-1)[:, np.newaxis] + epsilon
-    return x / sums
-
-
-def sample(pdfs):
-    max_words, vocab_size = pdfs.shape
-    samples = np.zeros(max_words)
-    for i in range(len(samples)):
-        samples[i] = np.random.choice(np.arange(vocab_size), p=pdfs[i])
-    return samples
-
-
 def build_model(encoder_dataset, decoder_dataset, **params):
     encoder = build_encoder(encoder_dataset, **params)
     decoder = build_decoder(decoder_dataset, **params)

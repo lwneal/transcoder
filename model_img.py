1ad86a876610f2e7b78c99a46f6419d6ce9e311e
Larry Neal <nealla@lwneal.com>
2017-05-05 17:20:31 -0700

    Add image model and image utils

---
+++ model_img.py
@@ -0,0 +1,56 @@
+import os
+import sys
+import random
+import numpy as np
+from keras import layers, models, applications
+import tensorflow as tf
+
+IMG_CHANNELS = 3
+IMG_HEIGHT = IMG_WIDTH = 224
+
+
+def build_encoder(dataset, **params):
+    THOUGHT_VECTOR_SIZE = params['thought_vector_size']
+    BATCH_SIZE = params['batch_size']
+    CNN = 'vgg16'
+    INCLUDE_TOP = True
+    LEARNABLE_CNN_LAYERS = 1
+    ACTIVATION = 'relu'
+
+    if CNN == 'vgg16':
+        cnn = applications.vgg16.VGG16(include_top=INCLUDE_TOP)
+    elif CNN == 'resnet':
+        cnn = applications.resnet50.ResNet50(include_top=INCLUDE_TOP)
+        # Pop the mean pooling layer
+        cnn = models.Model(inputs=cnn.inputs, outputs=cnn.layers[-2].output)
+
+    for layer in cnn.layers[:-LEARNABLE_CNN_LAYERS]:
+        layer.trainable = False
+
+    # Context Vector input
+    # normalized to [0,1] the values:
+    # left, top, right, bottom, (box area / image area)
+    input_ctx = layers.Input(batch_shape=(BATCH_SIZE, 5))
+    ctx = layers.BatchNormalization()(input_ctx)
+
+    # Global Image featuers (convnet output for the whole image)
+    input_img_global = layers.Input(batch_shape=(BATCH_SIZE, IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS))
+    image_global = cnn(input_img_global)
+
+    #image_global = layers.Lambda(lambda x: tf.reduce_mean(x, axis=1))(image_global)
+    #image_global = layers.Lambda(lambda x: tf.reduce_mean(x, axis=1))(image_global)
+
+    # Local Image featuers (convnet output for just the bounding box)
+    input_img_local = layers.Input(batch_shape=(BATCH_SIZE, IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS))
+    image_local = cnn(input_img_local)
+
+    #image_local = layers.Lambda(lambda x: tf.reduce_mean(x, axis=1))(image_local)
+    #image_local = layers.Lambda(lambda x: tf.reduce_mean(x, axis=1))(image_local)
+
+    x = layers.Concatenate()([image_global, image_local, ctx])
+    x = layers.Dense(THOUGHT_VECTOR_SIZE)(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation('relu')(x)
+    x = layers.Dense(THOUGHT_VECTOR_SIZE)(x)
+
+    return models.Model(inputs=[input_img_global, input_img_local, input_ctx], outputs=x)

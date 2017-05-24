a0f358cc265d1ed382bd244ca2c0f1819f8a1130
Lawrence Neal <nealla@oregonstate.edu>
2017-05-24 16:11:23 -0400

    Use the same pretrained network for D as G

--- model_img.py
+++ model_img.py
@@ -87,7 +87,7 @@ def build_decoder(**params):
 
     #x = layers.Conv2DTranspose(3, (3,3), strides=(2,2), padding='same')(x)
     x = layers.UpSampling2D()(x)
-    #x = SpatialCGRU(x, 3)
+    x = SpatialCGRU(x, 32)
     x = layers.Conv2D(3, (3,3), padding='same')(x)
     x = layers.Activation('sigmoid')(x)
 
@@ -95,25 +95,44 @@ def build_decoder(**params):
 
 
 def build_discriminator(**params):
+    thought_vector_size = params['thought_vector_size']
     batch_size = params['batch_size']
-    batch_input_shape = (batch_size, IMG_HEIGHT, IMG_WIDTH, 3)
+    pretrained_encoder = params['pretrained_encoder']
 
-    img_input = layers.Input(batch_shape=batch_input_shape)
-    x = layers.Conv2D(64, (3,3), padding='same')(img_input)
-    #x = layers.BatchNormalization()(x)
-    x = layers.Activation(LeakyReLU())(x)
-    x = layers.MaxPooling2D()(x)
-    x = layers.Conv2D(128, (3,3), padding='same')(x)
-    x = layers.BatchNormalization()(x)
-    x = layers.Activation(LeakyReLU())(x)
-    x = layers.MaxPooling2D()(x)
-    x = layers.Conv2D(256, (3,3), padding='same')(x)
-    x = layers.BatchNormalization()(x)
-    x = layers.Activation(LeakyReLU())(x)
-    x = layers.MaxPooling2D()(x)
+    include_top = False
+    LEARNABLE_CNN_LAYERS = 1
+
+    input_batch_shape = (batch_size, IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS)
+    input_img = layers.Input(batch_shape=input_batch_shape)
+    if pretrained_encoder:
+        if pretrained_encoder == 'vgg16':
+            cnn = applications.vgg16.VGG16(input_tensor=input_img, include_top=include_top)
+        elif pretrained_encoder == 'resnet50':
+            # Note: This is a hacked version of resnet50 with pooling removed
+            cnn = resnet50.ResNet50(include_top=include_top, pooling=None)
+        for layer in cnn.layers[:-LEARNABLE_CNN_LAYERS]:
+            layer.trainable = False
+        x = cnn(input_img)
+    else:
+        x = layers.Conv2D(64, (3,3), padding='same')(input_img)
+        x = layers.BatchNormalization()(x)
+        x = layers.Activation(LeakyReLU())(x)
+        x = layers.MaxPooling2D()(x)
+        x = layers.Conv2D(128, (3,3), padding='same')(x)
+        x = layers.BatchNormalization()(x)
+        x = layers.Activation(LeakyReLU())(x)
+        x = layers.MaxPooling2D()(x)
+        x = layers.Conv2D(256, (3,3), padding='same')(x)
+        x = layers.BatchNormalization()(x)
+        x = layers.Activation(LeakyReLU())(x)
+        x = layers.MaxPooling2D()(x)
+        x = layers.Conv2D(256, (3,3), padding='same')(x)
+        x = layers.BatchNormalization()(x)
+        x = layers.Activation(LeakyReLU())(x)
+        x = layers.MaxPooling2D()(x)
+
     x = layers.Flatten()(x)
-    x = layers.Dense(128, activation=LeakyReLU())(x)
-    x = layers.Dense(1, activation='tanh')(x)
-
-    return models.Model(inputs=img_input, outputs=x)
+    x = layers.Dense(1)(x)
+    x = layers.BatchNormalization()(x)
+    return models.Model(inputs=[input_img], outputs=x)
 

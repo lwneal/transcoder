d8cef9790b806dde072fcc7c8a73be8ab73f80a3
Larry Neal <nealla@lwneal.com>
2017-06-23 01:00:39 -0700

    Update CUB200 download script, add folds
    One test fold with a random 20%, the rest is training

--- scripts/download_cub200.py
+++ scripts/download_cub200.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 # Downloads the CUB200 dataset
 import os
+import random
 import json
 from subprocess import check_output
 
+
 DATA_DIR = os.path.expanduser('~/data')
 CUB_DIR = os.path.join(DATA_DIR, 'cub200')
+
 
 def mkdir(path):
     path = os.path.expanduser(path)
@@ -13,8 +16,10 @@ def mkdir(path):
         print('Creating directory {}'.format(path))
         os.mkdir(path)
 
+
 def filesize(filename):
     return os.stat(filename).st_size
+
 
 def download(filename, url):
     if os.path.exists(filename):
@@ -24,28 +29,20 @@ def download(filename, url):
         if url.endswith('.tgz') or url.endswith('.tar.gz'):
             os.system('ls *gz | xargs -n 1 tar xzvf')
 
+
 def load_mat_file(filename):
     import scipy.io as sio
     mat = sio.loadmat(filename)
     return mat['seg']
 
-if __name__ == '__main__':
-    mkdir(DATA_DIR)
-    mkdir(CUB_DIR)
-    os.chdir(CUB_DIR)
 
-    # Download and extract dataset
-    download('images', 'http://www.vision.caltech.edu/visipedia-data/CUB-200/images.tgz')
-    download('lists', 'http://www.vision.caltech.edu/visipedia-data/CUB-200/lists.tgz')
-    download('annotations-mat', 'http://www.vision.caltech.edu/visipedia-data/CUB-200/annotations.tgz')
-    download('attributes', 'http://www.vision.caltech.edu/visipedia-data/CUB-200/attributes.tgz')
-
-    # Generate CSV files
-    text = check_output('find images | grep -v "\.\_" | grep jpg$', shell=True)
-    images = text.splitlines()
-
-    print("Writing {} items to cub200.dataset".format(len(images)))
-    fp = open('{}/cub200.dataset'.format(DATA_DIR), 'w')
+def save_image_dataset(images, fold_name=None):
+    if fold_name:
+        output_filename = '{}/cub200_{}.dataset'.format(DATA_DIR, fold_name)
+    else:
+        output_filename = '{}/cub200.dataset'.format(DATA_DIR)
+    print("Writing {} items to {}".format(len(images), output_filename))
+    fp = open(output_filename, 'w')
     for filename in images:
         label = filename.lstrip('cub200/images/').split('/')[0].split('.')[-1]
         line = json.dumps({
@@ -55,3 +52,33 @@ if __name__ == '__main__':
         fp.write(line + '\n')
     fp.close()
     print("CUB200 dataset ready")
+
+
+if __name__ == '__main__':
+    print("CUB200 dataset download script initializing...")
+    mkdir(DATA_DIR)
+    mkdir(CUB_DIR)
+    os.chdir(CUB_DIR)
+
+    # Download and extract dataset
+    print("Downloading CUB200 dataset files...")
+    download('images', 'http://www.vision.caltech.edu/visipedia-data/CUB-200/images.tgz')
+    download('lists', 'http://www.vision.caltech.edu/visipedia-data/CUB-200/lists.tgz')
+    download('annotations-mat', 'http://www.vision.caltech.edu/visipedia-data/CUB-200/annotations.tgz')
+    download('attributes', 'http://www.vision.caltech.edu/visipedia-data/CUB-200/attributes.tgz')
+
+    # Generate CSV files
+    print("Generating Transcoder format CUB200 files")
+    text = check_output('find images | grep -v "\.\_" | grep jpg$', shell=True)
+    images = text.splitlines()
+    save_image_dataset(images)
+
+    # Split into a training and a test set
+    TRAIN_TEST_SPLIT = .8
+    split_idx = int(float(len(images)) * TRAIN_TEST_SPLIT)
+    random.shuffle(images)
+    train_images, test_images = images[:split_idx], images[split_idx:]
+
+    save_image_dataset(train_images, "train")
+    save_image_dataset(test_images, "test")
+    print("Finished building CUB200 dataset")

9d18a610900db20171f1d69d9ebf4c727bfa0001
Larry Neal <nealla@lwneal.com>
2017-06-29 21:55:11 -0700

    Combine MNIST and CUB200 experiments into one
    The counterfactual.sh script works for any dataset that:
    
        - Has a download_name.py script
        - Includes a _train and _test dataset file
        - Includes .img and .lab

---
+++ scripts/counterfactual.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+set -e
+set -x
+
+DATASET=$1
+ENCODER=$2
+DECODER=$3
+CLASSIFIER=$4
+THOUGHT_VECTOR_SIZE=$5
+EPOCHS=$6
+
+EXPERIMENT_NAME=${DATASET}_${THOUGHT_VECTOR_SIZE}_${ENCODER}_${DECODER}_`date +%s`
+
+if [[ $# -lt 6 ]]; then
+    echo "Usage: $0 <dataset> <encoder> <decoder> <classifier> <latent_size> <epochs>"
+    exit
+fi
+
+scripts/download_${DATASET}.py
+
+# First train a manifold
+python main.py \
+ --experiment-name $EXPERIMENT_NAME \
+ --encoder-input-filename ~/data/${DATASET}_train.dataset \
+ --decoder-input-filename ~/data/${DATASET}_train.dataset \
+ --classifier-input-filename ~/data/${DATASET}_train.dataset \
+ --encoder-datatype img \
+ --decoder-datatype img \
+ --classifier-datatype lab \
+ --encoder-model $ENCODER \
+ --decoder-model $DECODER \
+ --classifier-model $CLASSIFIER \
+ --discriminator-model $ENCODER \
+ --thought-vector-size $THOUGHT_VECTOR_SIZE \
+ --epochs $EPOCHS \
+ --batches-per-epoch 200 \
+ --stdout-filename train_aae.txt \
+ --enable-classifier True \
+ --mode train
+
+# Evaluate the classifier
+# Uses classifier.h5 as the decoder, then disables the classifier
+python main.py \
+ --experiment-name $EXPERIMENT_NAME \
+ --encoder-input-filename ~/data/${DATASET}_test.dataset \
+ --decoder-input-filename ~/data/${DATASET}_test.dataset \
+ --vocabulary-filename ~/data/${DATASET}_train.dataset \
+ --encoder-datatype img \
+ --decoder-datatype lab \
+ --decoder-weights classifier_${CLASSIFIER}.h5 \
+ --encoder-model $ENCODER \
+ --decoder-model $CLASSIFIER \
+ --thought-vector-size $THOUGHT_VECTOR_SIZE \
+ --enable-gan False \
+ --enable-classifier False \
+ --stdout-filename evaluate_classifier.txt \
+ --mode evaluate
+
+# Generate a "dream" video
+python main.py \
+ --experiment-name $EXPERIMENT_NAME \
+ --encoder-input-filename ~/data/${DATASET}_test.dataset \
+ --decoder-input-filename ~/data/${DATASET}_test.dataset \
+ --vocabulary-filename ~/data/${DATASET}_train.dataset \
+ --encoder-datatype img \
+ --decoder-datatype img \
+ --encoder-model $ENCODER \
+ --decoder-model $DECODER \
+ --thought-vector-size $THOUGHT_VECTOR_SIZE \
+ --enable-gan False \
+ --stdout-filename dream.txt \
+ --video-filename dream_output.mjpeg \
+ --mode dream
+
+# Re-encode the video to mp4 for storage
+pushd ~/results/$EXPERIMENT_NAME
+ffmpeg -i dream_output.mjpeg dream.mp4
+rm dream_output.mjpeg
+popd
+
+# Add counterfactuals
+for i in `seq 10`; do 
+    python main.py \
+     --experiment-name $EXPERIMENT_NAME \
+     --encoder-input-filename ~/data/${DATASET}_test.dataset \
+     --decoder-input-filename ~/data/${DATASET}_test.dataset \
+     --classifier-input-filename ~/data/${DATASET}_test.dataset \
+     --vocabulary-filename ~/data/${DATASET}_train.dataset \
+     --encoder-datatype img \
+     --decoder-datatype img \
+     --classifier-datatype lab \
+     --encoder-model $ENCODER \
+     --decoder-model $DECODER \
+     --classifier-model $CLASSIFIER \
+     --thought-vector-size $THOUGHT_VECTOR_SIZE \
+     --enable-gan False \
+     --enable-classifier True \
+     --stdout-filename counterfactual.txt \
+     --mode counterfactual
+done

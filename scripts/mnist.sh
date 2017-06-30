65f7975a3bf0a9f77227e246aab3ec8e56d93b8b
Larry Neal <nealla@lwneal.com>
2017-06-29 18:26:54 -0700

    Add options to MNIST script

--- scripts/mnist.sh
+++ scripts/mnist.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
+set -e
 
-EXPERIMENT_NAME=mnist_32dim_`date +%s`
+ENCODER_MODEL=$1
+DECODER_MODEL=$2
+CLASSIFIER_MODEL=$3
+THOUGHT_VECTOR_SIZE=$3
+EXPERIMENT_NAME=mnist_${THOUGHT_VECTOR_SIZE}_${ENCODER_MODEL}_${DECODER_MODEL}_`date +%s`
+
+if [[ $# -lt 3 ]]; then
+    echo "Usage: $0 <encoder_model> <decoder_model> <thought_vector_size>"
+    exit
+fi
 
 scripts/download_mnist.py
 
@@ -9,35 +19,20 @@ python main.py \
  --experiment-name $EXPERIMENT_NAME \
  --encoder-input-filename ~/data/mnist_train.dataset \
  --decoder-input-filename ~/data/mnist_train.dataset \
+ --classifier-input-filename ~/data/mnist_train.dataset \
  --encoder-datatype img \
  --decoder-datatype img \
- --encoder-model simplecnn_7a \
- --decoder-model simpledeconv_a \
- --discriminator-model simplecnn_7a \
- --thought-vector-size 32 \
+ --encoder-model $ENCODER_MODEL \
+ --decoder-model $DECODER_MODEL \
+ --classifier-model $CLASSIFIER_MODEL \
+ --discriminator-model $ENCODER_MODEL \
+ --thought-vector-size $THOUGHT_VECTOR_SIZE \
  --epochs 10 \
  --batches-per-epoch 200 \
  --stdout-filename train_aae.txt \
  --mode train
 
-# Now train a classifier keeping the encoder fixed
-python main.py \
- --experiment-name $EXPERIMENT_NAME \
- --encoder-input-filename ~/data/mnist_train.dataset \
- --decoder-input-filename ~/data/mnist_train.dataset \
- --encoder-datatype img \
- --decoder-datatype lab \
- --encoder-model simplecnn_7a \
- --decoder-model label_decoder \
- --thought-vector-size 32 \
- --freeze-encoder True \
- --enable-gan False \
- --epochs 10 \
- --batches-per-epoch 200 \
- --stdout-filename train_classifier.txt \
- --mode train
-
-# Now evaluate the classifier
+# Evaluate the classifier
 python main.py \
  --experiment-name $EXPERIMENT_NAME \
  --encoder-input-filename ~/data/mnist_test.dataset \
@@ -45,9 +40,9 @@ python main.py \
  --vocabulary-filename ~/data/mnist_train.dataset \
  --encoder-datatype img \
  --decoder-datatype lab \
- --encoder-model simplecnn_7a \
- --decoder-model label_decoder \
- --thought-vector-size 32 \
+ --encoder-model $ENCODER_MODEL \
+ --decoder-model $CLASSIFIER_MODEL \
+ --thought-vector-size $THOUGHT_VECTOR_SIZE \
  --enable-gan False \
  --stdout-filename evaluate_classifier.txt \
  --mode evaluate
@@ -60,9 +55,9 @@ python main.py \
  --vocabulary-filename ~/data/mnist_train.dataset \
  --encoder-datatype img \
  --decoder-datatype img \
- --encoder-model simplecnn_7a \
- --decoder-model simpledeconv_a \
- --thought-vector-size 32 \
+ --encoder-model $ENCODER_MODEL \
+ --decoder-model $DECODER_MODEL \
+ --thought-vector-size $THOUGHT_VECTOR_SIZE \
  --enable-gan False \
  --stdout-filename dream.txt \
  --video-filename dream_output.mjpeg \
@@ -72,3 +67,20 @@ python main.py \
 cd ~/results/$EXPERIMENT_NAME
 ffmpeg -i dream_output.mjpeg dream.mp4
 rm dream_output.mjpeg
+
+# Add counterfactuals
+for i in `seq 10`; do 
+    python main.py \
+     --experiment-name $EXPERIMENT_NAME \
+     --encoder-input-filename ~/data/mnist_test.dataset \
+     --decoder-input-filename ~/data/mnist_test.dataset \
+     --vocabulary-filename ~/data/mnist_train.dataset \
+     --encoder-datatype img \
+     --decoder-datatype lab \
+     --encoder-model $ENCODER_MODEL \
+     --decoder-model $DECODER_MODEL \
+     --thought-vector-size $THOUGHT_VECTOR_SIZE \
+     --enable-gan False \
+     --stdout-filename counterfactual.txt \
+     --mode counterfactual
+done

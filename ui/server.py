273a108db1cfc315cd4b16978145fbf8692c3b0c
Larry Neal <nealla@lwneal.com>
2017-07-27 11:18:03 -0700

    UI: Select from among multiple random .mp4 files

--- ui/server.py
+++ ui/server.py
@@ -1,10 +1,22 @@
 import flask
+import os
+import random
 
 app = flask.Flask(__name__)
 
 @app.route('/')
 def route_main_page():
-    return flask.render_template('index.html')
+    filenames = os.listdir('static')
+    filenames = [f for f in filenames if f.endswith('.mp4')]
+    if len(filenames) == 0:
+        raise ValueError("Error: No .mp4 files available in static/")
+    filename = random.choice(filenames)
+    class_name = filename.split('-')[-1].replace('.mp4', '')
+    args = {
+            'filename': filename,
+            'class': class_name,
+    }
+    return flask.render_template('index.html', **args)
 
 @app.route('/static/<path:path>')
 def serve_static():
@@ -12,7 +24,9 @@ def serve_static():
 
 @app.route('/submit', methods=['POST'])
 def submit_value():
-    print(flask.request.values)
+    print("Submitted value:")
+    for k, v in flask.request.form.items():
+        print("\t{}: {}".format(k, v))
     return flask.redirect('/')
 
 if __name__ == '__main__':

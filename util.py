534ab6171e0095dfd572a2e9469becd1e0a96f52
Larry Neal <nealla@lwneal.com>
2017-08-15 11:24:08 -0700

    Refactor file and logging functions to util.py
    Eventually util.py can be split into eg. fileutil, logutil, etc
    
    In my view, functions belong in a "utility" module iff they depend
    only on built-in modules and could feasibly be re-used elsewhere.

--- util.py
+++ util.py
@@ -1,5 +1,9 @@
 from contextlib import contextmanager
 import os
+import sys
+import unicodedata
+import re
+
 
 @contextmanager
 def pushd(newDir):
@@ -8,3 +12,34 @@ def pushd(newDir):
     yield
     os.chdir(previousDir)
 
+
+def chdir_to_experiment(experiment_name):
+    name = slugify(unicode(experiment_name))
+    os.chdir(os.path.expanduser('~/results'))
+    if not os.path.exists(name):
+        os.mkdir(name)
+    os.chdir(name)
+
+
+def slugify(value):
+    """
+    Normalizes string, converts to lowercase, removes non-alpha characters,
+    and converts spaces to hyphens.
+    """
+    value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore')
+    value = unicode(re.sub('[^\w\s-]', '', value).strip().lower())
+    return unicode(re.sub('[-\s]+', '-', value))
+
+
+def redirect_stdout_stderr(name):
+    class Logger(object):
+        def __init__(self, name='stdout.log'):
+            self.terminal = sys.stdout
+            self.log = open(name, "a")
+
+        def write(self, message):
+            self.terminal.write(message)
+            self.log.write(message)
+
+    if name:
+        sys.stdout = Logger(name)

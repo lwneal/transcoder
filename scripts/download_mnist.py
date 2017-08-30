b689662ce3bf52c7a66f2ab57409ba563195a5fe
Larry Neal <nealla@lwneal.com>
2017-08-30 13:43:18 -0700

    Improve MNIST download script
    Skip download if dataset already exists, add tqdm, add mnist.dataset

--- scripts/download_mnist.py
+++ scripts/download_mnist.py
@@ -1,21 +1,27 @@
 #!/usr/bin/env python
+import tqdm
 import os
 import json
 from keras import datasets
 from PIL import Image
+from tqdm import tqdm
 
 DATA_DIR = os.path.expanduser('~/data')
 
 def save_set(fold, x, y, suffix='png'):
+    examples = []
     fp = open('mnist_{}.dataset'.format(fold), 'w')
-    for i in range(len(x)):
+    print("Writing MNIST dataset {}".format(fold))
+    for i in tqdm(range(len(x))):
         label = y[i]
         img_filename = 'mnist/{}/{:05d}_{:d}.{}'.format(fold, i, label, suffix)
         Image.fromarray(x[i]).save(img_filename)
         entry = {'filename': img_filename, 'label': str(label)}
+        examples.append(entry)
         fp.write(json.dumps(entry))
         fp.write('\n')
     fp.close()
+    return examples
     
 
 if __name__ == '__main__':
@@ -24,8 +30,16 @@ if __name__ == '__main__':
         os.mkdir('mnist')
         os.mkdir('mnist/train')
         os.mkdir('mnist/test')
-    except:
+    except OSError:
         print("MNIST dataset exists at {}/mnist".format(DATA_DIR))
+        exit()
     (train_x, train_y), (test_x, test_y) = datasets.mnist.load_data()
-    save_set('train', train_x, train_y)
-    save_set('test', test_x, test_y)
+    train = save_set('train', train_x, train_y)
+    test = save_set('test', test_x, test_y)
+    for example in train:
+        example['fold'] = 'train'
+    for example in test:
+        example['fold'] = 'test'
+    with open('mnist.dataset', 'w') as fp:
+        for example in train + test:
+            fp.write(json.dumps(example) + '\n')

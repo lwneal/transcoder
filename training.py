f4167b14f29309947922840cf31116bb4f731364
Larry Neal <nealla@lwneal.com>
2017-09-01 11:47:02 -0700

    Switch from normal to uniform distribution
    It seems like uniform distributions are more common in GAN papers

--- training.py
+++ training.py
@@ -107,7 +107,7 @@ def train(models, datasets, **params):
         if enable_discriminator:
             for _ in range(discriminator_iters):
                 # Think some random thoughts
-                X_decoder = np.random.normal(0, 1, size=(batch_size, thought_vector_size))
+                X_decoder = np.random.uniform(-1, 1, size=(batch_size, thought_vector_size))
 
                 # Decode those random thoughts into hallucinations
                 X_generated = decoder.predict(X_decoder)
